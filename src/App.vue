20492872926572425111fc34a86f01b7c2cdd074
isaacgannon <36811304+isaacgannon@users.noreply.github.com>
2021-02-25 12:18:20 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/product-list-one.vue';
+import ProductListTwo from './components/product-list-two.vue';
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      
+     
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
